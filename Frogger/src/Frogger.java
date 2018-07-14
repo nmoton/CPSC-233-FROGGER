7c50f53e4f4b82c048bcd3b692acf320a4fd4a1c
Nathan Moton <26640786+nmoton@users.noreply.github.com>
2018-07-13 22:52:02 -0600

    Fixed screen resolution. Implementing Gameplay boilerplate

--- Frogger/src/Frogger.java
+++ Frogger/src/Frogger.java
@@ -1,17 +1,20 @@
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
-public class Frogger {
+public class Frogger{
 	
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		
-		SwingUtilities.invokeLater(new Runnable( ) {
-			public void run() {
-				JFrame frame = new JFrame("Frogger");
-				frame.setBounds(10,  10, 700, 600);
-				frame.setResizable(false);
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				frame.setVisible(true);
+		SwingUtilities.invokeLater(new Runnable( ){
+			public void run(){
+				JFrame gameWindow = new JFrame("Frogger");
+				Gameplay gameContent = new Gameplay();
+				
+				gameWindow.setBounds(50, 50, 640, 480);
+				gameWindow.setResizable(false);
+				gameWindow.setVisible(true);
+				gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				gameWindow.add(gameContent);
 			}
 		});
 	}
