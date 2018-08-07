bb8e8cdb736c181fab75b7e3d89f5aedca38ad1b
Justin Flores <40843059+PheeeG@users.noreply.github.com>
2018-08-06 22:25:24 -0600

    Update Frog.java

--- Frogger/src/Frog.java
+++ Frogger/src/Frog.java
@@ -4,6 +4,8 @@ public class Frog {
 	
 	private double playerPosX;
 	private double playerPosY;
+	private char lastFacing = 'U';
+
 	
 	public Frog(double setPlayerPosX, double setPlayerPosY) {
 		setPosX(setPlayerPosX);
@@ -12,18 +14,22 @@ public class Frog {
 	
 	public void moveUp() {
 		this.playerPosY -= 32;
+		this.lastFacing = 'U';
 	}
 	
 	public void moveDown() {
 		this.playerPosY += 32;
+		this.lastFacing = 'D';
 	}
 	
 	public void moveLeft() {
 		this.playerPosX -= 32;
+		this.lastFacing = 'L';
 	}
 	
 	public void moveRight() {
 		this.playerPosX += 32;
+		this.lastFacing = 'R';
 	}
 	
 	public void setPosX(double posX) {
@@ -40,5 +46,9 @@ public class Frog {
 	
 	public double getPlayerPosY() {
 		return playerPosY;
+	}
+	
+	public char getLastFacing() {
+		return this.lastFacing;
 	}
 }
