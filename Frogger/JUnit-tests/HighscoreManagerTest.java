0135352d6eb6c815bc69a63a0df5c0b1cb555e6b
Justin Flores <40843059+PheeeG@users.noreply.github.com>
2018-08-14 12:54:27 -0600

    Update HighscoreManagerTest.java

--- Frogger/JUnit-tests/HighscoreManagerTest.java
+++ Frogger/JUnit-tests/HighscoreManagerTest.java
@@ -6,7 +6,18 @@ import java.util.ArrayList;
 import org.junit.Test;
 
 public class HighscoreManagerTest {
-
+/**
+ * Note:
+ * - The highscore system should not display the scores in a 
+ * descending order
+ * -Repeated scores are shown
+ */
+	
+	/**
+	 * This test case checks if the sortScore() method can
+	 * properly sort the Score objects with different scores 
+	 * in a descending order.
+	 */
 	@Test
 	public void test_sortScores_differentScores() {
 		Score s1 = new Score(9, "EEE");
@@ -23,7 +34,7 @@ public class HighscoreManagerTest {
 		hm.addScoreToList(s5);
 
 		hm.sortScores();
-		ArrayList<Score> testList = new ArrayList<Score>(hm.copyList());
+		ArrayList<Score> testList = new ArrayList<Score>(hm.copyList());//Copies the assorted arrayList in to a different list
 
 		assertEquals("AAA(0) - score: ", 13, testList.get(0).getScore());
 		assertEquals("BBB(1) - score: ", 12, testList.get(1).getScore());
@@ -32,6 +43,12 @@ public class HighscoreManagerTest {
 		assertEquals("EEE(4) - score: ", 9, testList.get(4).getScore());
 	}
 
+	/**
+	 * This test case checks if the sortScore() method can
+	 * properly sort the Score objects when there are two
+	 * score object with the same score while the others
+	 * have different scores.
+	 */
 	@Test
 	public void test_sortScores_twoSameScores() {
 		Score s1 = new Score(11, "EEE");
@@ -48,7 +65,7 @@ public class HighscoreManagerTest {
 		hm.addScoreToList(s5);
 
 		hm.sortScores();
-		ArrayList<Score> testList = new ArrayList<Score>(hm.copyList());
+		ArrayList<Score> testList = new ArrayList<Score>(hm.copyList());//Copies the assorted arrayList in to a different list
 
 		assertEquals("AAA(0) - score: ", 13, testList.get(0).getScore());
 		assertEquals("BBB(1) - score: ", 12, testList.get(1).getScore());
@@ -57,6 +74,11 @@ public class HighscoreManagerTest {
 		assertEquals("DDD(4) - score: ", 10, testList.get(4).getScore());
 	}
 
+	/**
+	 * This test case checks if the sortScore() method can
+	 * properly sort the Score objects when all the Score objects
+	 * have the same score.
+	 */
 	@Test
 	public void test_sortScores_allSameScores() {
 		Score s1 = new Score(11, "EEE");
@@ -73,7 +95,7 @@ public class HighscoreManagerTest {
 		hm.addScoreToList(s5);
 
 		hm.sortScores();
-		ArrayList<Score> testList = new ArrayList<Score>(hm.copyList());
+		ArrayList<Score> testList = new ArrayList<Score>(hm.copyList());//Copies the assorted arrayList in to a different list
 
 		assertEquals("EEE(0) - score: ", 11, testList.get(0).getScore());
 		assertEquals("BBB(1) - score: ", 11, testList.get(1).getScore());
