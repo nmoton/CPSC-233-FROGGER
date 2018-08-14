2fc005b5f2d573a671b3535f86958e30e2be86f3
MikleM20 <40878852+MikleM20@users.noreply.github.com>
2018-08-14 14:45:32 -0600

    Update to not using username

--- Frogger/JUnit-tests/Score.java
+++ Frogger/JUnit-tests/Score.java
@@ -1,78 +1,88 @@
-
-
 import java.io.Serializable;
 import java.util.*;
 
 /**
  * @author Justin Flores
  *
+ *An object that holds the score and name of player. It can also update the score
+ *of the player.
+ *
+ * Last Updated:07/08/2018
  */
 public class Score implements Serializable {
-
+	
 	private static final long serialVersionUID = 1L;
-	private int score;
-	private String username;
+	private int score = 0;
 	private double highestPosY;
-	private static Scanner keyboard = new Scanner(System.in);
 
+	/**
+	 * Initializes the name, score, and highest y-position that
+	 * the player has achieved
+	 * @param posY - starting y position of the player
+	 */
 	Score(double posY){
 		score = 0;
-		username = null;
 		setHighestPosY(posY);
 	}
-
+	
 	//Only used for JUnit Testing
-	Score(int s, String name){
+	Score(int s){
 		score = s;
-		username = name;
-		setHighestPosY(0);
+		setHighestPosY(480);
 	}
-
+	
+	/**
+	 * A copy constructor, which copies the name and score of a Score object
+	 * @param toCopy - that object that is being copied
+	 */
 	Score(Score toCopy){
-		this.score = toCopy.score;
-		this.username = toCopy.username;
+		this.score = toCopy.getScore();
 	}
-
+	
+	/**
+	 * Description: returns score of the player
+	 * @return - score that the player got in
+	 * one run
+	 */
 	public int getScore() {
 		return score;
 	}
-
-	public String getUsername() {
-		return username;
+	
+	public String getScoreString() {
+		return new String(Integer.toString(this.score));
 	}
-
+	
+	/**
+	 * Sets the highest y-position that the player reached, which is used to
+	 * update the score of the player It can also be used to reset the highest 
+	 * y-position when going to a new level
+	 * @param posY - the highest y- position that the player reached
+	 */
 	public void setHighestPosY(double posY) {
 		highestPosY = posY;
 	}
+	
 
-	public void setUsername() {
-		Boolean flag = false;
-		String userInput = null;
-		while(flag == false) {
-			System.out.println("Enter your username: ");
-			userInput = keyboard.nextLine();
-			if(userInput.trim().equals("")) {
-				System.out.println("You must enter a username!");
-			}else {
-				username = userInput;
-				flag = true;
-			}
-		}
-	}
-
-	public void updateScore(double playerPosY) {
+	/**
+	 * Increases the players position if it reaches a y position higher than its
+	 * highestPosY, and updates the highestPosY to the new highest y position reached.
+	 * @param playerPosY
+	 */
+	public String updateScore(double playerPosY) {
+		String scoreAsString;
 		if(playerPosY < highestPosY) {
 			score += 10;
 			highestPosY = playerPosY;
 		}
-		System.out.println("Score: " + score + " Y: " + playerPosY + " highestY: " + highestPosY);
+		scoreAsString = (String)(Integer.toString(this.score));
+		return scoreAsString;
 	}
-
+	
+	/**
+	 * Increases the score if the player clears a level
+	 */
 	public void clearLevelScore() {
 		score += 1000;
-		System.out.println(score);
 	}
-
-
-
 }
+
