65e3638c480f8d24c23379804bc07fced386ab84
Nathan Moton <26640786+nmoton@users.noreply.github.com>
2018-08-14 14:12:39 -0600

    Commented the Frogger class

--- Frogger/src/Frogger.java
+++ Frogger/src/Frogger.java
@@ -6,13 +6,21 @@ import java.io.IOException;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
+/**
+ * @author Nathan Moton
+ *
+ * The Frogger class creates the game window in Swing and creates the game engine that will essentially run the entire
+ * game. The game engine's data is then added to the game window.
+ *
+ * Last Updated:08/14/2018
+ */
 public class Frogger {
 	
 	public static void main(String[] args) throws IOException{
 		
 		SwingUtilities.invokeLater(new Runnable( ){
 			public void run(){
-				JFrame gameWindow = new JFrame("Frogger");
+				JFrame gameWindow = new JFrame("Frogger"); //Names the window "Frogger"
 				
 				gameWindow.getContentPane().setPreferredSize((new Dimension(640, 480))); //Sets content pane to 640x480.
 				gameWindow.setResizable(false); //Keeps window at same size.
@@ -21,8 +29,8 @@ public class Frogger {
 				gameWindow.setLocationRelativeTo(null); //Forces frame to appear in middle of user's screen.
 				gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Terminates entire program upon closing window.
 				
-				Gameplay frogger = new Gameplay();
-				gameWindow.add(frogger);
+				Gameplay frogger = new Gameplay(); //Creates a new game engine that will manage the entire game
+				gameWindow.add(frogger); //Adds all the data of the game engine to the game window
 			}
 		});
 	}
