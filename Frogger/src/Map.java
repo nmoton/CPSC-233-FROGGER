b30e4e9e3b2c4e53dc4691d9b532d53127276410
Nathan Moton <26640786+nmoton@users.noreply.github.com>
2018-08-14 13:51:43 -0600

    Documentation for the map class

--- Frogger/src/Map.java
+++ Frogger/src/Map.java
@@ -1,10 +1,19 @@
 package frogger;
 
+/**
+ * @author Nathan Moton
+ * @editors Michael Manila
+ *
+ * The map class is a boilerplate that deals entirely with all the characteristics of maps. This includes the frog's starting position,
+ * the end zone boundaries, and initializes the graphics engine that deals with drawing the objects based on the characteristics of the map.
+ *
+ * Last Updated:08/14/2018
+ */
 public class Map{
 /**
- * Please refer to documentation on the grid system to design your own maps.
  * 
  * CHANGE THESE VALUES BELOW TO SELECT MAP CHARACTERISTICS:
+ * 
  */
 	
 	//Frog's starting position:
@@ -17,9 +26,10 @@ public class Map{
 	private int endZoneXBoundary2 = 346;
 	private int endZoneYBoundary2 = 64;
 	
-	//Frog for testing (LEAVE DEFAULT):
+	//Frog's initial location:
 	public Frog frog = new Frog(frogStartingX, frogStartingY);
 	
+	//Vehicle, log, and turtle arrays (LEAVE DEFAULT):
 	public Vehicle[][] vehicleArray;
 	public Log[][] logArray; 
 	public Turtle[][] turtleArray;
@@ -28,11 +38,17 @@ public class Map{
 	public GameGraphics graphicsEngine = new GameGraphics();
 
 /**
- * IGNORE THE FOLLOWING FUNCTIONS BELOW:	
+ * IGNORE THE FOLLOWING METHODS BELOW:	
  */
+	//Unused default constructor
 	public Map(){
 	}
 	
+	/**
+	 * Method that utilizes memory leaks to provide the graphics engine with the required data for the frog,
+	 * logs, vehicles, and turtles. This allows the graphics engine to read data from the same objects and arrays
+	 * updated by the game engine every game tick.
+	 */
 	public void MapGraphics(){
 		graphicsEngine.getUserFrog(this.frog);
 		graphicsEngine.getMapLogs(this.logArray);
@@ -40,18 +56,34 @@ public class Map{
 		graphicsEngine.getMapTurtles(this.turtleArray);
 	}
 	
+	/**
+	 * Getter method that returns the x coordinate of the first vertex of the end zone.
+	 * @return, returns the x coordinate of the first vertex of the end zone.
+	 */
 	public int getEndZoneXBoundary1(){
 		return this.endZoneXBoundary1;
 	}
 	
+	/**
+	 * Getter method that returns the y coordinate of the first vertex of the end zone.
+	 * @return, returns the y coordinate of the first vertex of the end zone.
+	 */
 	public int getEndZoneYBoundary1() {
 		return this.endZoneYBoundary1;
 	}
 	
+	/**
+	 * Getter method that returns the x coordinate of the second vertex of the end zone.
+	 * @return, returns the x coordinate of the second vertex of the end zone.
+	 */
 	public int getEndZoneXBoundary2() {
 		return this.endZoneXBoundary2;
 	}
 	
+	/**
+	 * Getter method that returns the y coordinate of the second vertex of the end zone.
+	 * @return, returns the y coordinate of the second vertex of the end zone.
+	 */
 	public int getEndZoneYBoundary2() {
 		return this.endZoneYBoundary2;
 	}
